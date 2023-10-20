234e9d9ff9e47b92b815e483c9883fef209d20bd
AygyunS <32463645+AygyunS@users.noreply.github.com>
2023-10-20 17:27:27 +0300

    Last azure-resume changes small not important

--- frontend/main.js
+++ frontend/main.js
@@ -2,16 +2,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApiUrl = 'https://getresumecounter11.azurewebsites.net'
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCount';
 
 const getVisitCount = () => {
     let count = 10
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
-    }).then(response => {
-        console.log("Website called function API.")
+    }).then(response =>{
+        console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText - count
+        document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
     });
